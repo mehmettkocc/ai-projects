26627d7091227c498f0bce31dc32429383d0ec7c
Mehmet K <mehmettkocc@gmail.com>
2014-02-08 18:27:38 -0800

    getBestLabelSequence and getScoreMatrix modified

---
+++ 2/getScoreMatrix.m
@@ -0,0 +1,21 @@
+%{
+Author: 
+Date:
+//////////////////////////////////////////////////////////////////////////
+Description: getScoreMatrix(x, w) is a function which has 2 arguments:
+x = the example to calculate a score for using w
+w = the weight vector for features
+
+
+
+
+%}
+
+function G = getScoreMatrix(x, w)
+% calculate m and n
+% m???
+n = length(x);
+G = zeros(m, m, n-1);
+
+
+end
