0e39451fb0b42d71e2482c2acaabe40b242b1628
zeynepsu <zskurultay@gmail.com>
2014-02-12 10:49:38 -0800

    feats

--- 2/lowFeatures.m
+++ 2/lowFeatures.m
@@ -2,20 +2,20 @@ function fj = lowFeatures(yi,yii,x,k)
 %LOWFEATURES This function computes f_j.
 %   Detailed explanation goes here
 
-j = 9; % number of feature functions
+j = 10; % number of feature functions
 %ySet = {'COMMA', 'PERIOD' , 'QUESTION_MARK', 'EXCLAMATION_POINT', 'COLON', 'SPACE'};
 fj = zeros(j,1);
-fj(1) = ffTempA(x,k,'dt')*ffTempB(yi,6);
-fj(2) = ffTempA(x,1,'wrb')*ffTempB(yi,3);
-fj(3) = ffTempA(x,k,'prp')*ffTempB(yi,6);
-fj(4) = ffTempA(x,k,'jjr')*ffTempB(yi,6);
-fj(5) = ffTempA(x,k,'vbd')*ffTempB(yi,6);
-fj(6) = ffTempA(x,1,'md')*ffTempB(yi,3);
-fj(7) = ffTempA(x,1,'vbp')*ffTempB(yi,3);
-fj(8) = ffTempA(x,1,'wp')*ffTempB(yi,3);
-fj(9) = ffTempA(x,1,'prp')*ffTempB(yi,6);
-%dummy features instead of having 36*36*2*2 feature functions for speed at
-%this initial stage
+fj(1) = ffTempA2(x,k,{'DT','CC','JJ','PRP','JJR','LS','TO','POS','VBD','IN'})*ffTempB(yi,6);
+fj(2) = ffTempA2(x,1,{'WRB','WP','WDT','WP$','MD','VBP'})*ffTempB3(yi,3,x,k);
+fj(3) = ffTempA(x,k,'PRP')*ffTempB(yii,1);
+fj(4) = ffTempA(x,k,'CC')*ffTempB(yii,1);
+fj(5) = ffTempA(x,k,'RB')*ffTempB(yi,1);
+fj(6) = ffTempA3(x,k)*ffTempB(yi,6);
+fj(7) = ffTempA3(x,k)*ffTempB(yi,1);
+fj(8) = ffTempA3(x,k)*ffTempB(yi,5);
+fj(9) = ffTempA3(x,k)*ffTempB(yi,2);
+fj(10) = ffTempA4(x,k,'VB','IN')*ffTempB(yi,6);
+
 end
 %feature function templates
 function b = ffTempB(yi,label)
@@ -26,10 +26,26 @@ function b = ffTempB2(yi,yii,labeli,labelii)
     b = ((yi==labeli)&&(yii==labelii));
 end
 
+function b = ffTempB3(yi,label,x,k)
+    b = ((yi==label)&&(k==length(x{1})));
+end
+
 function a = ffTempA(x,k,posTag)
     a = strcmpi(x{1}(k),posTag);
 end
 
 function a = ffTempA2(x,k,posGroup)
     a = sum(ismember(posGroup,x{1}(k)));
+end
+
+function a = ffTempA3(x,k)
+    a = (length(x{1})==k);
+end
+
+function a = ffTempA4(x,k,pos1,pos2)
+    if(k~=1)
+        a = (strcmpi(x{1}(k),pos1)&&strcmpi(x{1}(k-1),pos2));
+    else
+        a = 0;
+    end
 end
