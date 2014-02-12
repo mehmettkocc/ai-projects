7fe8b642f42632bdfde89ddcc25800bcb78e04a4
zeynepsu <zskurultay@gmail.com>
2014-02-11 19:30:15 -0800

    now final

--- 2/readInp.m
+++ 2/readInp.m
@@ -20,7 +20,7 @@ fclose(fid);
 for k=1:size(res,1)
   Xtest{k}=str2num(res(k,:));
 end
-X = importdata('data/posTest.txt');
+X = importdata('data/posTraining.txt');
 actX = cell(size(X,1),1);
 for k=1:size(X,1)
    sd = textscan(X{k,1},'%s');
@@ -28,3 +28,13 @@ for k=1:size(X,1)
   % data = data';
    actX{k} = data;
 end
+X = actX;
+Xtest = importdata('data/posTest.txt');
+actX = cell(size(Xtest,1),1);
+for k=1:size(Xtest,1)
+   sd = textscan(Xtest{k,1},'%s');
+   data = cellstr(sd{1});
+  % data = data';
+   actX{k} = data;
+end
+Xtest = actX;
